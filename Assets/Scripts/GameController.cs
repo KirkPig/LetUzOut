cc632a2f727eb8b4cd0549b9a58ad811e4314879
NatapongS <natapong099@gmail.com>
2021-02-06 11:52:31 +0700

    Add Radio and GameController

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController: MonoBehaviour
+{
+    // Start is called before the first frame update
+    public RadioScript radio;
+    public bool isEnd;
+    void Start()
+    {
+        isEnd = false;
+        radio = FindObjectOfType<RadioScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isEnd) return;
+        if (radio.health == 0) { isEnd = true; Debug.Log("YOU LOSE"); }
+        else if (radio.progress == 100) { isEnd = true; Debug.Log("YOU WIN"); }
+    }
+}
---
+++ Assets/Scripts/RadioScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int health;
+    public int progress;
+    public GameController gameController;
+    void Start()
+    {
+        gameController = FindObjectOfType<GameController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Zombie")
+        {
+            health -= collision.gameObject.GetComponent<ZombieScript>().attack;
+            if (health < 0) health = 0;
+        }
+    }
+}
