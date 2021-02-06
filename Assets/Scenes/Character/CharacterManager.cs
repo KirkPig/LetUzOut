dac41f23ce38e8e807da75c9aa78ace5ca2246ab
oJadeo <jade.jiravate@gmail.com>
2021-02-06 11:52:41 +0700

    character

---
+++ Assets/Scenes/Character/CharacterManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour
+{
+    public Character character1;
+    public Character character2;
+    public Character character3;
+    public Character character4;
+    private Character selectedCharacter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    [SerializeField]
+    private Camera cam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit  = Physics2D.Raycast(mousePos, Vector2.zero);
+            if(hit)
+            {
+                IClickable clickable = hit.collider.GetComponent<IClickable>();
+                clickable.click();
+            }
+            else{
+                if(selectedCharacter!=null){
+                    selectedCharacter.setMoveTargetPosition();
+                }
+            }
+        }
+    }
+
+    public void setSelectedCharacter(Character targetCharacter){
+        selectedCharacter = targetCharacter;
+    }
+}
